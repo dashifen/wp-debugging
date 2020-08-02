ace122de19a558bf2596d809986b70359a4d7026
David Dashifen Kees <dashifen@dashifen.com>
2020-08-02 11:02:40 -0400

    initial commit

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Dashifen\WP-Debugging
+
+A small extension of my general PHP [on-screen debugging repository](https://github.com/dashifen/debugging) that works specifically within a WordPress environment.
---
+++ src/WPDebuggingTrait.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Dashifen\WPDebugging;
+
+use Dashifen\Debugging\DebuggingTrait;
+
+trait WPDebuggingTrait
+{
+  use DebuggingTrait;
+  
+  /**
+   * isDebug
+   *
+   * Returns true if either the conditions of the DebuggingTrait are met or
+   * if our WordPress environment has its WP_DEBUG flag set.
+   *
+   * @return bool
+   */
+  public static function isDebug(): bool
+  {
+    return DebuggingTrait::isDebug() || (defined('WP_DEBUG') && WP_DEBUG);
+  }
+  
+  /**
+   * writeLog
+   *
+   * Calling this method should write $data to the WordPress debug.log file.
+   *
+   * @param mixed $data
+   *
+   * @return void
+   */
+  public static function writeLog($data): void
+  {
+    // source:  https://www.elegantthemes.com/blog/tips-tricks/using-the-wordpress-debug-log
+    // accessed:  2020-08-02
+    
+    if (!function_exists("write_log")) {
+      function write_log($log)
+      {
+        if (is_array($log) || is_object($log)) {
+          error_log(print_r($log, true));
+        } else {
+          error_log($log);
+        }
+      }
+    }
+    
+    write_log($data);
+  }
+}
